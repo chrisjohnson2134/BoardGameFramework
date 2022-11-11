7190e20fba088094cb88c4637446a5ccaa970e46
chrisjohnson2134 <chrisjohnson7690@gmail.com>
2022-11-11 15:20:19 -0600

    Setup Maui Project

---
+++ GamePlayer.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GamePlayer.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GamePlayer.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GamePlayer.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
